3a0fe6b355ba58321f78ceb61d5242d4b7690a30
tonborrell <tonborrell@gmail.com>
2023-04-18 16:47:48 +0200

    small modifications

--- app/db_conn.py
+++ app/db_conn.py
@@ -1,13 +1,21 @@
 from db.db_connection import get_db_connection
 
+
 def db_conn():
     return get_db_connection()
 
+
 def get_users_collection():
-    return db_conn()['users']
+    return db_conn()["users"]
+
 
 def get_products_collection():
-    return db_conn()['products']
+    return db_conn()["products"]
+
+
+def get_categories_collection():
+    return db_conn()["categories"]
+
 
 def get_product_rating_collection():
-    return db_conn()['product_rating']
+    return db_conn()["product_rating"]
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from app.db_conn import get_categories_collection, get_products_collection
 from db.db_connection import get_db_connection
 from db.add_products import add_products_from_json
 from db.create_update_categories import add_categories, update_categories
@@ -7,8 +8,8 @@ from db.create_update_categories import add_categories, update_categories
 # result = update_categories(categories_collection=categories_collection, category_name='Guitarras y accesorios', category_url='https://www.amazon.es/gp/bestsellers/musical-instruments/4965355031/ref=zg_bs_nav_musical-instruments_1')
 
 # result = add_products_from_json(
-#     product_collection=products_collection,
-#     categories_collection=categories_collection,
+#     product_collection=get_products_collection(),
+#     categories_collection=get_categories_collection(),
 #     json_file_path="db/json_products/guitars_bestsellers.json",
 # )
 
