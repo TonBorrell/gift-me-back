9a70278d128af1aa37d972a6177f830b5ed20970
tonborrell <tonborrell@gmail.com>
2023-04-18 19:24:56 +0200

    commit

--- app/routers/login.py
+++ app/routers/login.py
@@ -7,24 +7,29 @@ from ..db_conn import get_users_collection
 from db.manage_users import check_user
 
 router = APIRouter(
-    prefix='/login',
-    tags=['login'],
-    responses={404: {"description": "Not found"}}
+    prefix="/api/v1/login",
+    tags=["login"],
+    responses={404: {"description": "Not found"}},
 )
+
 
 class User(BaseModel):
     username: str
     password: str
 
+
 def verify_password(plain_password, hashed_password):
     pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
     return pwd_context.verify(plain_password, hashed_password)
 
-@router.post('')
+
+@router.post("")
 async def register_user(user: User):
-    user_db = check_user(users_collection=get_users_collection(), username=user.username)
+    user_db = check_user(
+        users_collection=get_users_collection(), username=user.username
+    )
     if user_db:
-        is_password_correct = verify_password(user.password, user_db['password'])
+        is_password_correct = verify_password(user.password, user_db["password"])
         if is_password_correct:
             return {"Logged in": True}
         else:
@@ -32,4 +37,3 @@ async def register_user(user: User):
     elif user_db == None:
         return {"Logged in": False}
     raise HTTPException(status_code=404, detail="User not found")
-
--- app/routers/product_rating.py
+++ app/routers/product_rating.py
@@ -8,48 +8,51 @@ from db.product_rating import set_product_rating_in_db
 from ..db_conn import get_products_collection, get_product_rating_collection
 
 router = APIRouter(
-    prefix='/product_rating',
-    tags=['product_rating'],
-    responses={404: {"description": "Not found"}}
+    prefix="/api/v1/product_rating",
+    tags=["product_rating"],
+    responses={404: {"description": "Not found"}},
 )
+
 
 class Rating(BaseModel):
     asin: str
     rating: int
 
-@router.get('/')
+
+@router.get("/")
 def get_products_to_rate() -> list:
     products = get_products(get_products_collection())
     response = []
     for prod in products:
-        prod.pop('_id', None)
+        prod.pop("_id", None)
         response.append(prod)
-    
+
     random_response = random.choices(response, k=10)
 
     return random_response
 
-@router.get('/one')
+
+@router.get("/one")
 def get_products_to_rate() -> list:
     products = get_products(get_products_collection())
     response = []
     for prod in products:
-        prod.pop('_id', None)
+        prod.pop("_id", None)
         response.append(prod)
-    
+
     random_response = random.choices(response, k=1)
 
     return random_response
 
-@router.post('/')
+
+@router.post("/")
 def set_product_rating(rating: Dict[str, str]):
     print(rating)
     for asin, rate in rating.items():
         print(asin, rate)
-    return {'success': 'all rating wrote on db'}
+    return {"success": "all rating wrote on db"}
     """
     for prod_rating in rating:
         set_product_rating_in_db(product_collection=get_products_collection(), product_rating_collection=get_product_rating_collection(), rating=prod_rating.dict())
     return {'success': 'all rating wrote on db'}
     """
-
