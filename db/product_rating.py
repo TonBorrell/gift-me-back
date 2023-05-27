6ae31f1e6ac155d1af612d27823cfb5485ae9aaf
tonborrell <tonborrell@gmail.com>
2023-05-27 13:53:37 +0200

    enable endpoint rating and correctly set results

--- db/product_rating.py
+++ db/product_rating.py
@@ -1,9 +1,18 @@
 import pymongo.collection
 
 from .db_schema import rating_schema
-from .add_products import is_product_in_db_by_id 
+from .add_products import is_product_in_db_by_asin
 
-def set_product_rating_in_db(product_collection: pymongo.collection.Collection, product_rating_collection: pymongo.collection.Collection, rating: dict):
+
+def set_product_rating_in_db(
+    product_rating_collection: pymongo.collection.Collection,
+    rating: dict,
+):
     rating_schema.validate(rating)
-    if is_product_in_db_by_id(product_collection, rating['product_id']):
-        product_rating_collection.insert_one(rating)
+    product_rating_collection.insert_one(rating)
+
+
+def delete_product_rating_in_db(
+    product_rating_collection: pymongo.collection.Collection,
+):
+    product_rating_collection.delete_many({})
